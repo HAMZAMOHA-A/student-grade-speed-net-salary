1dd147a2b7a486b1c058982d16b6adc8278fcb12
HAMZAMOHA-A <hamzashukri29@gmail.com>
2024-09-27 16:55:19 +0300

    Update speedDetector.js

--- speedDetector.js
+++ speedDetector.js
@@ -1,13 +1,18 @@
+
 function checkSpeed(speed) {
     const speedLimit = 70;
     if (speed < speedLimit) {
         return "Ok";
-    } else {
-        let demeritPoints = Math.floor((speed - speedLimit) / 5);
-        if (demeritPoints > 12) {
-            return "License suspended";
-        } else {
-            return `Points: ${demeritPoints}`;
-        }
     }
+    const demeritPoints = Math.floor((speed - speedLimit) / 5);
+    if (demeritPoints > 12) {
+        return "License suspended";
+    }
+    return `Points: ${demeritPoints}`;
+}
+
+
+const speed = prompt("Enter car speed:");
+console.log(checkSpeed(Number(speed)));
+
 }
