2b8d96c76c1e8a8f03f0311ed39b89cf106b9b4b
HAMZAMOHA-A <hamzashukri29@gmail.com>
2024-09-27 16:54:18 +0300

    Update studentGradeGenerator.js

--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -1,4 +1,4 @@
-// Student Grade Generator
+
 function getGrade(marks) {
     if (marks < 0 || marks > 100) {
         return "Invalid marks. Please enter a number between 0 and 100.";
@@ -10,6 +10,6 @@ function getGrade(marks) {
     return 'E';
 }
 
-// Example usage
+
 const marks = prompt("Enter student marks (0-100):");
 console.log(getGrade(Number(marks)));
